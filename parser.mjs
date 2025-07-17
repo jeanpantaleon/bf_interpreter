6a522c14e29b59ea46cf0417b84268417a928a1c
Jean Pantaléon <jepantal@outlook.fr>
2025-07-17 11:02:50 +0200

    feat: different files for each functionnality

---
+++ parser.mjs
@@ -0,0 +1,120 @@
+import { Token } from "./tokens.mjs";
+
+export class Parser {
+    tokens = [];
+
+    pointerPosition = 0;
+	memorySize = 30;
+    memory = Array(this.memorySize).fill(0);
+
+    tokenize = (input) => {
+        let chars = input.split("");
+
+        while (chars.length > 0) {
+            let char = chars.shift();
+            if (/\+/g.test(char)) {
+                this.tokens.push(Token.PLUS);
+            } else if (/\-/g.test(char)) {
+                this.tokens.push(Token.MINUS);
+            } else if (/\</g.test(char)) {
+                this.tokens.push(Token.LEFT);
+            } else if (/\>/g.test(char)) {
+                this.tokens.push(Token.RIGHT);
+            } else if (/\./g.test(char)) {
+                this.tokens.push(Token.DISPLAY);
+            } else if (/\[/g.test(char)) {
+                this.tokens.push(Token.LOOP_START);
+            } else if (/\]/g.test(char)) {
+                this.tokens.push(Token.LOOP_END);
+            } else if (/\s|\t|\n/g.test(char)) {
+                continue;
+            } else {
+                throw new Error(`Unknown token: ${char}`);
+            }
+        }
+        return this.tokens;
+    };
+
+    /**
+     * First function in the parsing tree
+     *
+     * @returns an int that represents the tokens eaten by the function
+     */
+    parseLoop = (tokenList, indexOfLoop) => {
+        if (tokenList[indexOfLoop] != Token.LOOP_START) {
+            throw `Parsing loop with a bad token: ${tokenList[indexOfLoop]}`;
+        }
+
+        let loop_tokens = [];
+        let indexInLoop = 1;
+
+        let numberOfLoopInside = 0;
+
+        while (
+            tokenList[indexOfLoop + indexInLoop] != Token.LOOP_END ||
+            numberOfLoopInside > 0
+        ) {
+            if (tokenList[indexOfLoop + indexInLoop] == Token.LOOP_START) {
+                numberOfLoopInside++;
+            } else if (tokenList[indexOfLoop + indexInLoop] == Token.LOOP_END) {
+                numberOfLoopInside--;
+            }
+            loop_tokens.push(tokenList[indexOfLoop + indexInLoop]);
+            indexInLoop++;
+        }
+
+        let localIndex = 0;
+        while (this.memory[this.pointerPosition] > 0) {
+            while (localIndex < loop_tokens.length) {
+                localIndex += this.parseAction(loop_tokens, localIndex);
+            }
+
+            localIndex = 0;
+        }
+
+        // + 2 because of the two [ and ] that are not included in the tokenList
+        return loop_tokens.length + 2;
+    };
+
+    parseAction = (tokenList, indexOfToken) => {
+        switch (tokenList[indexOfToken]) {
+            case Token.LOOP_START:
+                return this.parseLoop(tokenList, indexOfToken);
+            case Token.PLUS:
+                this.memory[this.pointerPosition] =
+                    ++this.memory[this.pointerPosition] % 255;
+                break;
+            case Token.MINUS:
+                this.memory[this.pointerPosition]--;
+                if (this.memory[this.pointerPosition] < 0)
+                    this.memory[this.pointerPosition] = 255;
+                break;
+            case Token.LEFT:
+                this.pointerPosition--;
+				if(this.pointerPosition < 0)
+					this.pointerPosition = this.memorySize - 1
+                break;
+            case Token.RIGHT:
+                this.pointerPosition = ++this.pointerPosition % this.memorySize
+                break;
+            case Token.DISPLAY:
+                console.log(this.memory[this.pointerPosition]);
+                break;
+        }
+
+        return 1;
+    };
+
+    interpret = () => {
+        let index = 0;
+        while (index < this.tokens.length) {
+            index += this.parseAction(this.tokens, index);
+        }
+
+        return this.memory;
+    };
+
+    printMemory = () => {
+        console.log(this.memory);
+    };
+}
---
+++ tokens.mjs
@@ -0,0 +1,10 @@
+export let Token;
+(function (Token) {
+    Token[(Token["PLUS"] = 0)] = "PLUS";
+    Token[(Token["MINUS"] = 1)] = "MINUS";
+    Token[(Token["LEFT"] = 2)] = "LEFT";
+    Token[(Token["RIGHT"] = 3)] = "RIGHT";
+    Token[(Token["DISPLAY"] = 4)] = "DISPLAY";
+    Token[(Token["LOOP_START"] = 5)] = "LOOP_START";
+    Token[(Token["LOOP_END"] = 6)] = "LOOP_END";
+})(Token || (Token = {}));

092672d0c1dc413c52dbada43584753d300d9c00
Jean Pantaléon <jepantal@outlook.fr>
2025-07-17 14:46:04 +0200

    feat: adding argument parsing

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 import { Parser } from "./parser/parser.mjs";
 import { readFileSync } from "fs";
 
-let codeFile = readFileSync("./scripts/test.bf", {encoding: "utf-8" })
+let codeFile;
+
+if (process.argv.length > 3) {
+    throw "Too many arguments"
+} else if(process.argv.length = 3) {
+    try {
+        codeFile = readFileSync(process.argv[2], {encoding: "utf-8" })
+    } catch (e) {
+        console.error(`${e}`)
+        process.exit(1)
+    }
+} else {
+    codeFile = readFileSync("./scripts/main.js", {encoding: "utf-8" })
+}
+
+
 
 let parser = new Parser();
 
